dfdd1871183b71e0f2cf4c1bec9e5988f99b2bbe
Abdeali <abdealichharchhoda786@gmail.com>
2023-11-05 12:23:36 +0530

    server run properly

--- src/main/java/dev/abdeation/moviesAPI/MoviesApiApplication.java
+++ src/main/java/dev/abdeation/moviesAPI/MoviesApiApplication.java
@@ -2,14 +2,21 @@ package dev.abdeation.moviesAPI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class MoviesApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MoviesApiApplication.class, args);
 	}
 
+	@GetMapping("/")
+	public String apiPath(){
+		return "Movies API listening on <a href=`http://localhost:8080/`>localhost:8080</a>";
+	}
 
 
 }
